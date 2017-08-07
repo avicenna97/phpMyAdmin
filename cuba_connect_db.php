0afc03b2c770252cc5fa4a0632f8f07a61c1bdcd
avicenna97 <30398110+avicenna97@users.noreply.github.com>
2017-08-07 16:52:49 +0800

    create cuba_connect_db.php

---
+++ cuba_connect_db.php
@@ -0,0 +1,20 @@
+<?php
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database */
+define('DB_NAME', $_ENV['OPENSHIFT_APP_NAME']);
+
+/** MySQL database username */
+define('DB_USER', $_ENV['OPENSHIFT_MYSQL_DB_USERNAME']);
+
+/** MySQL database password */
+define('DB_PASSWORD', $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']);
+
+/** MySQL hostname */
+define('DB_HOST', $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT']);
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+printf("MySQL server version: %s\n", mysql_get_server_info());
+?>
